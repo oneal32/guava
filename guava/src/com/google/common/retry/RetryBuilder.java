5b8e3c3130c701bfb2b9c77c672f26d626dd466b
jianwenchen <jianwenchen@sohu-inc.com>
2024-11-24 23:11:57 +0800

    init

--- guava/src/com/google/common/retry/RetryBuilder.java
+++ guava/src/com/google/common/retry/RetryBuilder.java
@@ -2,14 +2,36 @@ package com.google.common.retry;
 
 public class RetryBuilder {
 
+    private Retryer retryer;
+
+    private RetryBuilder(Retryer retryer){
+        this.retryer = retryer;
+    }
+    public static RetryBuilder newBuilder(){
+        return new RetryBuilder(new Retryer());
+    }
+
     public RetryBuilder retryIfException(Throwable e){
         return this;
     }
 
-    public RetryBuilder retryIf(RetryCondition condition);
+    public RetryBuilder retryIf(RetryCondition condition){
+        return this;
+    }
 
+    public RetryBuilder strategy(RetryStrategy strategy){
+        return this;
+    }
+
+    public Retryer build(){
+        return this.retryer;
+    }
 
     public static class RetryCondition {
+
+    }
+
+    public static class RetryStrategy {
 
     }
 }
